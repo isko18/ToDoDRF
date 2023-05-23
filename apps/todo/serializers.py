0aa5b8b8ee0a6e530d51b6efd811200836cb91b4
isko18 <toksonbaevislam2004@gmail.com>
2023-05-23 15:49:17 +0600

    users/todo

---
+++ apps/todo/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from apps.todo.models import ToDo
+
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = ('id', 'user', 'title', 'description',
+                 'created_at', 'image')
---
+++ apps/users/views.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from apps.users.models import User
+from apps.users.serializers import UserRegisterSerializer, UserSerializer, UserDetailSerializer
+
+# Create your views here.
+class UserAPIViewSet(GenericViewSet,
+                     ListModelMixin,
+                     RetrieveModelMixin,
+                     CreateModelMixin,
+                     UpdateModelMixin,
+                     DestroyModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('create', ):
+            return UserRegisterSerializer
+        elif self.action in ('retrieve', ):
+            return UserDetailSerializer
+        return UserSerializer
